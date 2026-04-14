23a8cbaaaca523b0f2eb9dbbfbc0d3c2a7b258cc
rainweaver76 <96443663+rainweaver76@users.noreply.github.com>
2026-04-14 00:23:22 -0400

    mounts of mayhem initial commit

---
+++ src/main/java/com/otterly76/ott/effect/BreathOfNautilusMobEffect.java
@@ -0,0 +1,11 @@
+package com.otterly76.ott.effect;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+
+/** Prevents the rider's oxygen bar from depleting while mounted on a nautilus. */
+public class BreathOfNautilusMobEffect extends MobEffect {
+    public BreathOfNautilusMobEffect(MobEffectCategory category, int color) {
+        super(category, color);
+    }
+}
